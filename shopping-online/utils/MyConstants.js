3a825b276755b97c3d61ea21438f02689cd17643
hung512 <55794001+hung512@users.noreply.github.com>
2020-06-19 00:13:09 +0700

    update code

---
+++ shopping-online/utils/MyConstants.js
@@ -0,0 +1,10 @@
+var MyConstants = {
+  DB_SERVER: 'clustersgp-i5upf.mongodb.net',
+  DB_USER: 'MinHun',
+  DB_PASS: 'kakalotkakalot',
+  DB_DATABASE: 'shoppingonline',
+  HOSTNAME: 'localhost:3000',
+  EMAIL_USER: 'hung.187pm06604@vanlanguni.vn',
+  EMAIL_PASS: 'VLU187pm06604',
+};
+module.exports = MyConstants;
